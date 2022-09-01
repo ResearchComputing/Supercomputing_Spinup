9144d15b4572567466ceffbba5510191ef4c39ab
monaghaa <33913248+monaghaa@users.noreply.github.com>
2022-09-01 12:50:58 -0600

    Update submit_test.sh

--- job_submission_spinup/alpine_scripts/submit_test.sh
+++ job_submission_spinup/alpine_scripts/submit_test.sh
@@ -3,7 +3,7 @@
 ## Directives
 #SBATCH --ntasks=1                               # Number of requested tasks
 #SBATCH --time=0:01:00                           # Max wall time
-#SBATCH --partition=amilan-ucb                   # Specify Summit Haswell nodes
+#SBATCH --partition=amilan                   # Specify Summit Haswell nodes
 #SBATCH --output=./output/test_%j.out            # Rename standard output file
 
 ## Software
