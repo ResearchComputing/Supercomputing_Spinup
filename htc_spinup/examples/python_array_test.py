05ffb399d593f0f139784e712e508915a113f19c
monaghaa <33913248+monaghaa@users.noreply.github.com>
2024-09-03 09:51:30 -0600

    Fall 2024 SC Spinup Part 1 slides (linux bash) (#7)
    * Delete htc_spinup directory
    
    * Delete linux_bash_spinup/old_slides directory
    
    * Delete linux_bash_spinup/linux-and-bash-spring-2024.pdf
    
    * Delete linux_bash_spinup/linux-and-bash-spring-2024.pptx
    
    * Fall 2024 slides, initial commit

--- htc_spinup/examples/python_array_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-print "python is opening the file ", sys.argv[1]
-f=open(sys.argv[1],"r") 
-print "and here's what is in it:"
-print"----------------------------"
-print(f.read())
-print"----------------------------"
-
